728bf3db4b05ba2611e7a355aa07ab8f65e9b2c9
yoon <cbj0010@naver.com>
2023-11-12 03:57:22 +0900

    [Feat] 입력받은 주문 수량체크
    무작정 구현해보기

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,42 @@
 package christmas
 
 fun main() {
-    TODO("프로그램 구현")
+//    dayOfWeek()
+    val input = "해산물파스타-2,레드와인-1"
+    val orders = OrderParse().parseOrder(input)
+
+    for (order in orders) {
+        println("주문 메뉴: ${order.menuName}, 수량: ${order.quantity}")
+    }
 }
+
+//입력한 요일을 판단하는 함수
+
+val userInput = InputView()
+
+data class Order(val menuName: String, val quantity: Int)
+
+class OrderParse(){
+    fun parseOrder(input: String): List<Order> {
+        val orderList = mutableListOf<Order>()
+
+        val menuOrders = input.split(",")
+
+        for (menuOrder in menuOrders) {
+            val (menuName, quantityStr) = menuOrder.split("-")
+
+            try {
+                val quantity = quantityStr.toInt()
+                orderList.add(Order(menuName.trim(), quantity))
+            } catch (e: NumberFormatException) {
+                // 예외 처리: 수량이 숫자로 변환할 수 없는 경우
+                println("수량을 올바르게 입력해주세요: $menuOrder")
+            }
+        }
+
+        return orderList
+    }
+
+}
+
+
---
+++ src/main/kotlin/christmas/InputView.kt
@@ -0,0 +1,7 @@
+package christmas
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun inputDay(): String = Console.readLine()
+}
