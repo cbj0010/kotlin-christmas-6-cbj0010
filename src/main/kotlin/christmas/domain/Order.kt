449ffebf290eefac4796bea8f2f1dcbeedde6fc7
yoon <cbj0010@naver.com>
2023-11-12 17:02:49 +0900

    [Style] 코드 정리

--- src/main/kotlin/christmas/domain/Order.kt
+++ src/main/kotlin/christmas/domain/Order.kt
@@ -1,5 +1,11 @@
 package christmas.domain
 
 data class Order(val menuName: String, val quantity: Int) {
+    /*
+    사용자가 입력한 값을 splite로 나눠서 저장하는 곳
+    티본스테이크 1개
+    바비큐립 1개
+    이런 형식
+     */
 //    init
 }
--- src/main/kotlin/christmas/domain/SaleCalculator.kt
+++ src/main/kotlin/christmas/domain/SaleCalculator.kt
@@ -1,13 +1,17 @@
 package christmas.domain
 
-import christmas.DayOfWeek
-import christmas.userInput
 import christmas.util.Appetizer
+import christmas.util.DayOfWeek
+import christmas.view.InputView
 
 class SaleCalculator {
+
+    //user가 입력한 값을 어떻게 들고 있을지 생각,
+    // 그래야 그거 기반으로 주말, 평일의 메뉴 할인을 어캐 적용할건지 달라지니까
+
     private fun dayOfWeek(): String {
         val dayOfWeek = mutableMapOf<DayOfWeek, Int>()
-        val day = userInput.inputDay().toInt()
+        val day = InputView().inputDay().toInt()
         return when (day % 7) {
             1, 2 -> "주말"
             else -> "평일"
@@ -27,7 +31,6 @@ class SaleCalculator {
 
         // 사용자 입력에 해당하는 Appetizer를 찾음
         val selectedMenu = Appetizer.values().find { it.appetizerName == userInput }
-
 
         // 선택된 메뉴에 따라 개수를 증가시킬 mutableMap 초기화
         val menuCountMap = mutableMapOf<Appetizer, Int>(
