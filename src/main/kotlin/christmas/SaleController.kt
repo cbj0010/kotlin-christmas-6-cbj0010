6e667760606f8202726a439417a3efc3592749c9
yoon <cbj0010@naver.com>
2023-11-12 19:43:01 +0900

    [Feat] 할인금액 정보 들고있기
    파일분리/메뉴마다의 할인 금액, 총액에 적용되야하는 할인금액의 정보를 들고 있는다

--- src/main/kotlin/christmas/SaleController.kt
+++ src/main/kotlin/christmas/SaleController.kt
@@ -1,10 +1,41 @@
 package christmas
 
-import christmas.view.InputView
+import christmas.data.ChristmasDiscountDayInfo
+import christmas.data.Order
+import christmas.domain.MenuSaleCalculator
+import christmas.domain.SumSaleCalculator
 
-class SaleController {
+class SaleController(private val inputDay: Int, private val menuList: List<Order>) {
 
-//입력한 요일을 판단하는 함수
+//입력한 요일을 판단하는 함수 , 검증 된 날짜와 검증된 Order(name,count)형식의 메뉴리스트를 받는다.
 
-    val userInput = InputView()
+    private fun checkDayForSumDiscount(): ChristmasDiscountDayInfo {
+        return when {
+            inputDay % 7 == 3 -> ChristmasDiscountDayInfo(STAR_DAY, inputDay)
+            inputDay < 25 -> ChristmasDiscountDayInfo(CHRISTMAS_EVE_DAY, inputDay)
+            inputDay == 25 -> ChristmasDiscountDayInfo(CHRISTMAS_DAY, 25)
+            else -> ChristmasDiscountDayInfo(NONE, 0)
+        }
+    }
+
+    private fun checkDayForMenuDiscount(): String {
+        return when (inputDay % 7) {
+            1, 2 -> "주말"
+            else -> "평일"
+        }
+    }
+
+    fun sumSaleMoney() {
+        val menuDiscountDay = checkDayForMenuDiscount()
+        val sumDiscountDay = checkDayForSumDiscount()
+        MenuSaleCalculator(menuDiscountDay).calculateDiscountRate(menuList)
+        SumSaleCalculator(sumDiscountDay).calculateDiscountSpecialDay()
+    }
+
+    companion object {
+        const val STAR_DAY = "STAR_DAY"
+        const val CHRISTMAS_DAY = "CHRISTMAS_DAY"
+        const val CHRISTMAS_EVE_DAY = "CHRISTMAS_EVE_DAY"
+        const val NONE = "NONE"
+    }
 }
