f550c7758737ec6cf0d2ded132806c97923911c3
yoon <cbj0010@naver.com>
2023-11-14 02:47:41 +0900

    [Test] 메뉴 조건 테스트
    메뉴가 중복인 경우

--- src/test/kotlin/christmas/MenuValidatorTest.kt
+++ src/test/kotlin/christmas/MenuValidatorTest.kt
@@ -14,13 +14,18 @@ class MenuValidatorTest {
 
     @Test
     fun `메뉴판 형식(메뉴 - 수량)에 안맞게 입력 했을 때 예외처리`() {
-        val selectMenus = "-1,제로콜라-1"
+        val selectMenus = "3, 제로콜라-a"
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
 
     @Test
-    fun `메뉴주문 수가 20개가 넘을 때`() {
+    fun `메뉴주문 수가 20개가 넘을 때 예외처리`() {
         val selectMenus = "티본스테이크-20,제로콜라-1"
+        assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
+    }
+    @Test
+    fun `메뉴주문이 중복일때 예외처리`() {
+        val selectMenus = "티본스테이크-2,티본스테이크-1,티본스테이크-3,티본스테이크-4"
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
 }
