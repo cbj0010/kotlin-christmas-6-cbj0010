4f6334abc6eb7e455be792ca69692ecfb4339f35
yoon <cbj0010@naver.com>
2023-11-13 20:17:31 +0900

    [Feat] 메뉴값이 잘 입력됐나 검증

--- src/main/kotlin/christmas/domain/MenuValidator.kt
+++ src/main/kotlin/christmas/domain/MenuValidator.kt
@@ -1,6 +1,33 @@
 package christmas.domain
 
-class MenuValidator {
+import christmas.util.*
 
-    //Order(1,1),Order(스테이크,1) 이런식으로 나눠준 애 중애서 메뉴가 메뉴판에 있는 메뉴인지 판별
+class MenuValidator(private val selectedMenu: String) {
+    //Order(1,1),Order(스테이크,1) 이런식으로 나눠준 애 중애서 메뉴가 메뉴판에 있는 메뉴인지 팔별
+    //1,1이런식으로 나눠진거는 메뉴 형식에 맞게 입력하지 않은거니까 예외를 던짐
+
+    //수량이 0 개 일 때 ERROR_MENU_INPUT던짐
+    init {
+        isValidMenu()
+    }
+
+
+    private fun isValidMenu() {
+        val parsedMenu = OrderParse().parseOrder(selectedMenu)
+        parsedMenu.forEach { order ->
+            require(!checkMenu(order.menuName)) { ErrorMessage.ERROR_MENU_INPUT.getMessage() }
+        }
+    }
+
+    private fun checkMenu(menuNameToCheck: String): Boolean {
+        //해당하는 메뉴가 없을 경우 ERROR_MENU_INPUT 던짐
+        val isMainValid = MainMenu.values().any { it.mainMenuName == menuNameToCheck }
+        val isDessertValid = Dessert.values().any { it.dessertName == menuNameToCheck }
+        val isAppetizerValid = Appetizer.values().any { it.appetizerName == menuNameToCheck }
+        val isDrinkValid = Beverage.values().any { it.drinkName == menuNameToCheck }
+
+        // 하나라도 일치하는 메뉴가 있다면 true, 그렇지 않으면 false 반환
+        return isMainValid || isDessertValid || isAppetizerValid || isDrinkValid
+
+    }
 }
