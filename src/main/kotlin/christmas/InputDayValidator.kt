7f6809a637dd3f68c31effabfee1045f268202d1
yoon <cbj0010@naver.com>
2023-11-14 03:05:21 +0900

    [Feat/docs] 입력날짜 조건 검사
    날짜가 숫자가 아닌경우, 1~31에 해당하는 숫자가 아닌경우 예외 던지기

---
+++ src/main/kotlin/christmas/InputDayValidator.kt
@@ -0,0 +1,24 @@
+package christmas
+
+import christmas.util.ErrorMessage
+
+class InputDayValidator(private val inputDay: String) {
+
+    fun isValidDate(): Int {
+        validateNatural()
+        rangeDate()
+        return inputDay.toInt()
+    }
+
+    private fun validateNatural() =
+        require(inputDay.toIntOrNull() == null) {
+            ErrorMessage.ERROR_INPUT_DAY.getMessage()
+        }
+
+    private fun rangeDate() {
+        require(inputDay.toInt() !in 1..31) {
+            ErrorMessage.ERROR_INPUT_DAY.getMessage()
+        }
+    }
+
+}
--- src/main/kotlin/christmas/util/ErrorMessage.kt
+++ src/main/kotlin/christmas/util/ErrorMessage.kt
@@ -1,7 +1,8 @@
 package christmas.util
 
 enum class ErrorMessage(private val message: String) {
-    ERROR_MENU_INPUT("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    ERROR_MENU_INPUT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ERROR_INPUT_DAY("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     fun getMessage(): String = "[ERROR] $message"
 }
