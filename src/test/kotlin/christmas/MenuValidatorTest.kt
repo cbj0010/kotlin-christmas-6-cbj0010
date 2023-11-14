e65ab761ff0c37620d1467945b15b9822da53068
yoon <cbj0010@naver.com>
2023-11-14 14:00:59 +0900

    [Test] 정상 메뉴입력값 테스트

--- src/test/kotlin/christmas/MenuValidatorTest.kt
+++ src/test/kotlin/christmas/MenuValidatorTest.kt
@@ -1,6 +1,8 @@
 package christmas
 
+import christmas.data.Order
 import christmas.domain.MenuValidator
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -14,7 +16,7 @@ class MenuValidatorTest {
 
     @Test
     fun `메뉴판 형식(메뉴 - 수량)에 안맞게 입력 했을 때 예외처리`() {
-        val selectMenus = "3, 제로콜라-a"
+        val selectMenus = "3, 제로콜라-a,5"
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
 
@@ -23,9 +25,21 @@ class MenuValidatorTest {
         val selectMenus = "티본스테이크-20,제로콜라-1"
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
+
     @Test
     fun `메뉴주문이 중복일때 예외처리`() {
         val selectMenus = "티본스테이크-2,티본스테이크-1,티본스테이크-3,티본스테이크-4"
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
+
+    @Test
+    fun `제대로 된 메뉴를 입력한 경우`() {
+        val selectMenus = "티본스테이크-19,제로콜라-1"
+        val parseMenu: List<Order> = listOf(
+            Order(menuName = "티본스테이크", quantity = 19),
+            Order(menuName = "제로콜라", quantity = 1)
+        )
+        assertThat(MenuValidator(selectMenus).isValidMenu()).isEqualTo(parseMenu)
+    }
+
 }
