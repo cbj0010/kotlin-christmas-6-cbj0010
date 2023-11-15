2e0113f1cecfadd7a2ad11bda9232f32ebe8988f
yoon <cbj0010@naver.com>
2023-11-15 14:26:29 +0900

    [Feat] 시작지점 구현

--- src/main/kotlin/christmas/FoodStore.kt
+++ src/main/kotlin/christmas/FoodStore.kt
@@ -1,26 +1,53 @@
 package christmas
 
-import christmas.data.Order
 import christmas.domain.CalculatorTotalPrice
+import christmas.domain.MenuValidator
+import christmas.view.InputView
+import christmas.view.OutputView
+
 
 class FoodStore() {
+    private val outputView = OutputView()
     fun start() {
-        val parseMenu: List<Order> = listOf(
-            Order(menuName = "티본스테이크", quantity = 1),
-            Order(menuName = "바비큐립", quantity = 1),
-            Order(menuName = "초코케이크", quantity = 2),
-            Order(menuName = "제로콜라", quantity = 1)
-        )
-        val date = "3"
-        val totalOrderPrice = CalculatorTotalPrice(parseMenu).checkSumOrderMoney()
-//할인 전 총 주문금액
-        println("할인 전 총 주문 금액 : $totalOrderPrice")
+        val inputUser = InputView().inputDay()
+        val date = InputDayValidator(inputUser).isValidDate()   //검증된 날짜
+        val menu = MenuValidator(InputView().inputDay()).isValidMenu()
+        val totalOrderPrice = CalculatorTotalPrice(menu).checkSumOrderMoney()
+        val totalDiscountPrice = SaleController(date, menu)
+        outputView.startMessage()
+
+        outputView.displayMenu(menu)
+
+        //할인 전 총 주문금액
+        outputView.displayTotalPriceOrder(totalOrderPrice)
+        //증정메뉴
+        showGiftEvent(ValidateEventConditions(totalOrderPrice).checkValidGift())
         //혜택
-        println("혜택 내역")
-        val totalDiscountPrice = SaleController(date.toInt(), parseMenu).sumSaleMoney()
-        println("총혜택 금액")
-        println(totalDiscountPrice)
+        showDiscount(totalOrderPrice, totalDiscountPrice)
+
         println("할인 후 예상 결제 금약")
-        println(totalOrderPrice - totalDiscountPrice)
+        println(totalOrderPrice - totalDiscountPrice.sumSaleMoney())
+
+        OutputView().displayChristMasBadge(totalDiscountPrice.sumSaleMoney())
+    }
+
+    fun showGiftEvent(isGift: Boolean) {
+        val outputView = OutputView()
+        when (isGift) {
+            true -> outputView.displayGiftEvent()
+            false -> outputView.displayGiftNoneEvent()
+        }
+    }
+
+    private fun showDiscount(totalPrice: Int, totalDiscountPrice: SaleController) {
+        //이게 true이면 sale이 들어감 false면 세일이 들어가지 않음을 보여줌
+        val outputView = OutputView()
+        val checkEvent = ValidateEventConditions(totalPrice)
+
+        if (checkEvent.checkEventRequirements()) {
+            totalDiscountPrice.saveDiscountInfo(totalPrice)
+        } else {
+            outputView.displayNoBenefits()
+        }
     }
 }
