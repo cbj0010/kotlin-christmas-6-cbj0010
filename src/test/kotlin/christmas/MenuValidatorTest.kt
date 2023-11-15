e70cfed8d4785e6956bbb92a637792a3a3bee03a
yoon <cbj0010@naver.com>
2023-11-15 17:01:36 +0900

    [Test] menu 유효성 및 수량 체크

--- src/test/kotlin/christmas/MenuValidatorTest.kt
+++ src/test/kotlin/christmas/MenuValidatorTest.kt
@@ -2,7 +2,6 @@ package christmas
 
 import christmas.data.Order
 import christmas.domain.MenuValidator
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -10,36 +9,28 @@ class MenuValidatorTest {
 
     @Test
     fun `메뉴판에 없는 메뉴를 입력 했을 때 예외처리`() {
-        val selectMenus = "꼼파뇨-1,제로콜라-1"
-        assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
-    }
-
-    @Test
-    fun `메뉴판 형식(메뉴 - 수량)에 안맞게 입력 했을 때 예외처리`() {
-        val selectMenus = "3, 제로콜라-a,5"
+        val selectMenus = listOf(
+            Order(menuName = "꼼파뇨", quantity = 1),
+            Order(menuName = "제로콜라", quantity = 1)
+        )
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
 
     @Test
     fun `메뉴주문 수가 20개가 넘을 때 예외처리`() {
-        val selectMenus = "티본스테이크-20,제로콜라-1"
+        val selectMenus = listOf(
+            Order(menuName = "티본스테이크", quantity = 20),
+            Order(menuName = "제로콜라", quantity = 1)
+        )
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
 
     @Test
     fun `메뉴주문이 중복일때 예외처리`() {
-        val selectMenus = "티본스테이크-2,티본스테이크-1,티본스테이크-3,티본스테이크-4"
+        val selectMenus = listOf(
+            Order(menuName = "티본스테이크", quantity = 1),
+            Order(menuName = "티본스테이크", quantity = 2)
+        )
         assertThrows<IllegalArgumentException> { MenuValidator(selectMenus).isValidMenu() }
     }
-
-    @Test
-    fun `제대로 된 메뉴를 입력한 경우`() {
-        val selectMenus = "티본스테이크-19,제로콜라-1"
-        val parseMenu: List<Order> = listOf(
-            Order(menuName = "티본스테이크", quantity = 19),
-            Order(menuName = "제로콜라", quantity = 1)
-        )
-        assertThat(MenuValidator(selectMenus).isValidMenu()).isEqualTo(parseMenu)
-    }
-
 }
