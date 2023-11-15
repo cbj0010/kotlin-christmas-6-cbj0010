2eb065e38e0a47d3659d6d5d7160126213f8a543
yoon <cbj0010@naver.com>
2023-11-15 14:26:18 +0900

    [Refactor] 증정정보 저장

--- src/main/kotlin/christmas/data/BenefitInfo.kt
+++ src/main/kotlin/christmas/data/BenefitInfo.kt
@@ -4,5 +4,6 @@ data class BenefitInfo(
     val christmasNearDiscount: Int,
     val menuDiscountDay: Int,
     val specialDiscountDay: Int,
+    val giftEventMoney: Int,
     val sumDiscountMoney: Int
 )
