4603054643856f36e51f2473357ab5a9d8fa32cc
yoon <cbj0010@naver.com>
2023-11-15 19:30:46 +0900

    [Refactor] 네이밍 수정 및 print분리

--- src/main/kotlin/christmas/FoodStore.kt
+++ src/main/kotlin/christmas/FoodStore.kt
@@ -17,17 +17,17 @@ class FoodStore() {
 //수량이 0 개 일 때 ERROR_MENU_INPUT던짐
         try {
             val date = InputDayValidator(InputView().inputUser()).isValidDate()
-            println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+            outputView.showOrderPrompt()
             val parsedMenu: List<Order> = OrderParse().parseOrder(InputView().inputUser())
             val menu = MenuValidator(parsedMenu).isValidMenu()
-            showAll(date, menu)
+            showOrderInfo(date, menu)
         } catch (e: IllegalArgumentException) {
             println(e.message)
             checkUserValidate()
         }
     }
 
-    private fun showAll(date: Int, menu: List<Order>) {
+    private fun showOrderInfo(date: Int, menu: List<Order>) {
         val totalOrderPrice = CalculatorTotalPrice(menu).checkSumOrderMoney()
         val totalDiscountPrice = SaleController(date, menu)
         val christMasBadge =
@@ -49,7 +49,6 @@ class FoodStore() {
     }
 
     private fun showGiftEvent(isGift: Boolean) {
-        val outputView = OutputView()
         when (isGift) {
             true -> outputView.displayGiftEvent()
             false -> outputView.displayGiftNoneEvent()
@@ -58,7 +57,6 @@ class FoodStore() {
 
     private fun showDiscount(totalPrice: Int, totalDiscountPrice: SaleController) {
         //이게 true이면 sale이 들어감 false면 세일이 들어가지 않음을 보여줌
-        val outputView = OutputView()
         val checkEvent = ValidateEventConditions(totalPrice)
 
         if (checkEvent.checkEventRequirements()) {
--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -10,6 +10,10 @@ class OutputView {
         println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
     }
 
+    fun showOrderPrompt(){
+        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+    }
+
     fun displayMenu(menu: List<Order>) {
         println()
         println("<주문 메뉴>")
